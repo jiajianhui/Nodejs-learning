0051ee523deb8970c6493cae458e7326d0d3e32c
jianhui <1164367974@qq.com>
2024-08-19 14:11:59 +0800

    删除账单

--- 05-记账本案例/routes/index.js
+++ 05-记账本案例/routes/index.js
@@ -31,7 +31,17 @@ router.post("/accountList", function (req, res, next) {
   // 写入数据
   db.get("account").unshift({ id: id, ...req.body}).write();
   // 动态设置信息
-  res.render("success", {msg: '添加', url: '/accountList'});
+  res.render("success", { msg: "添加成功", url: "/accountList" });
+});
+
+// 删除账单
+router.get("/accountList/:id", function (req, res, next) {
+  // 获取id
+  let id = req.params.id;
+  // 删除数据
+  db.get("account").remove({ id: id }).write();
+  // 动态设置信息
+  res.render("success", { msg: "删除成功", url: "/accountList" });
 });
 
 module.exports = router;
