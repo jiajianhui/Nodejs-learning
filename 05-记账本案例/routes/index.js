526c49a619fddee9cc74ae953fa2f84a29413d9f
jianhui <1164367974@qq.com>
2024-08-19 10:39:48 +0800

    使用lowdb保存账单信息

--- 05-记账本案例/routes/index.js
+++ 05-记账本案例/routes/index.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+// 导入lowdb
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+
+const adapter = new FileSync(__dirname + "/../data/db.json");
+
+// 获取db对象
+const db = low(adapter);
+
+// 导入shortid
+const shortid = require("shortid");
+
 // 账单列表
 router.get('/accountList', function(req, res, next) {
   res.render('list')
@@ -12,8 +24,10 @@ router.get("/accountList/creatAccount", function (req, res, next) {
 
 // 新增账单
 router.post("/accountList", function (req, res, next) {
-  // 获取请求体数据
-  console.log(req.body);
+  // 生成id
+  let id = shortid.generate()
+  // 写入数据
+  db.get("account").unshift({ id: id, ...req.body}).write();
   res.send('添加成功')
 });
 
